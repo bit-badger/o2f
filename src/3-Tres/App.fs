7927eb31859d749ef195cd01b81346c09209b424
Daniel J. Summers <daniel.summers.2415@gmail.com>
2019-07-21 22:03:54 -0500

    Start Step 1 Tres

---
+++ src/3-Tres/App.fs
@@ -0,0 +1,16 @@
+﻿namespace Tres
+
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Nancy.Owin
+
+type Startup() =
+  member __.Configure (app : IApplicationBuilder) =
+    app.UseOwin (fun x -> x.UseNancy (fun x -> ()) |> ignore) |> ignore
+
+module App = 
+  [<EntryPoint>]
+  let main argv = 
+    use host = (new WebHostBuilder()).UseKestrel().UseStartup<Startup>().Build ()
+    host.Run ()
+    0
